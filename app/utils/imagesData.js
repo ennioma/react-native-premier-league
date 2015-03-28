61249b570e7fb6edf677ad4dd68fd4173acf07d4
Ennio <ennio.masi@gmail.com>
2015-03-28 16:51:07 +0100

    Moved the teams png to a dedicated module

---
+++ app/utils/imagesData.js
@@ -0,0 +1,12 @@
+var images = {
+  'MUFC': 'http://img.skysports.com/football/badges/128/210.png',
+  'SWA': 'http://img.skysports.com/football/badges/128/375.png',
+  'LCFC': 'http://footytweets.com/images/team_logos/55.png',
+  'EFC': 'http://www.futbol24.com/upload/team/England/Everton-FC.png',
+  'WHU': 'http://www.futbol24.com/upload/team/England/West-Ham.png',
+  'THFC': 'http://www.calcioefinanza.it/wp-content/uploads/2013/08/Tottenham-Hotspur-icon.png',
+  'WBA': 'http://img.skysports.com/football/badges/128/275.png',
+  'SUN': 'http://pesstatsdatabase.com/PSD/PSD/Images/Clubs/England/Sunderland-AFC.png'
+}
+
+module.exports = images;
