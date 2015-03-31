48bf1f8a1188047b5712de8349d6b07a5b33be5c
Ennio <ennio.masi@gmail.com>
2015-03-31 17:41:42 +0200

    Updated team icons source

--- app/utils/imagesData.js
+++ app/utils/imagesData.js
@@ -1,12 +1,24 @@
 var images = {
-  'MUFC': 'http://img.skysports.com/football/badges/128/210.png',
-  'SWA': 'http://img.skysports.com/football/badges/128/375.png',
-  'LCFC': 'http://footytweets.com/images/team_logos/55.png',
-  'EFC': 'http://www.futbol24.com/upload/team/England/Everton-FC.png',
-  'WHU': 'http://www.futbol24.com/upload/team/England/West-Ham.png',
-  'THFC': 'http://www.calcioefinanza.it/wp-content/uploads/2013/08/Tottenham-Hotspur-icon.png',
-  'WBA': 'http://img.skysports.com/football/badges/128/275.png',
-  'SUN': 'http://pesstatsdatabase.com/PSD/PSD/Images/Clubs/England/Sunderland-AFC.png'
+  'MUFC': 'http://icons.iconarchive.com/icons/giannis-zographos/english-football-club/256/Manchester-United-icon.png',
+  'SWA': 'http://icons.iconarchive.com/icons/giannis-zographos/english-football-club/256/Swansea-City-icon.png',
+  'LCFC': 'http://icons.iconarchive.com/icons/giannis-zographos/english-football-club/256/Leicester-City-icon.png',
+  'EFC': 'http://icons.iconarchive.com/icons/giannis-zographos/english-football-club/256/Everton-FC-icon.png',
+  'WHU': 'http://icons.iconarchive.com/icons/giannis-zographos/english-football-club/256/West-Ham-United-icon.png',
+  'THFC': 'http://icons.iconarchive.com/icons/giannis-zographos/english-football-club/256/Tottenham-Hotspur-icon.png',
+  'WBA': 'http://icons.iconarchive.com/icons/giannis-zographos/english-football-club/256/West-Bromwich-Albion-icon.png',
+  'SUN': 'http://icons.iconarchive.com/icons/giannis-zographos/english-football-club/256/Sunderland-AFC-icon.png',
+  'SCFC': 'http://icons.iconarchive.com/icons/giannis-zographos/english-football-club/256/Stoke-City-icon.png',
+  'AVFC': 'http://icons.iconarchive.com/icons/giannis-zographos/english-football-club/256/Aston-Villa-icon.png',
+  'QPR': 'http://icons.iconarchive.com/icons/giannis-zographos/english-football-club/256/Queens-Park-Rangers-icon.png',
+  'AFC': 'http://icons.iconarchive.com/icons/giannis-zographos/english-football-club/256/Arsenal-FC-icon.png',
+  'CRY': 'http://icons.iconarchive.com/icons/giannis-zographos/english-football-club/256/Crystal-Palace-icon.png',
+  'LFC': 'http://icons.iconarchive.com/icons/giannis-zographos/english-football-club/256/Liverpool-FC-icon.png',
+  'SFC': 'http://icons.iconarchive.com/icons/giannis-zographos/english-football-club/256/Southampton-FC-icon.png',
+  'NUFC': 'http://icons.iconarchive.com/icons/giannis-zographos/english-football-club/256/Newcastle-United-icon.png',
+  'MCFC': 'http://icons.iconarchive.com/icons/giannis-zographos/english-football-club/256/Manchester-City-icon.png',
+  'BFC': 'http://icons.iconarchive.com/icons/giannis-zographos/british-football-club/256/Burnley-FC-icon.png',
+  'CFC': 'http://icons.iconarchive.com/icons/giannis-zographos/english-football-club/256/Chelsea-FC-icon.png',
+  'HUL': 'http://icons.iconarchive.com/icons/giannis-zographos/english-football-club/256/Hull-City-icon.png'
 }
 
 module.exports = images;
