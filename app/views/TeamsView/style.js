bb8326bcebb170b75218844f8ebd932028d371a3
Ennio <ennio.masi@gmail.com>
2015-04-01 09:25:59 +0200

    Old styles removed

--- app/views/TeamsView/style.js
+++ app/views/TeamsView/style.js
@@ -10,13 +10,6 @@ module.exports = StyleSheet.create({
   container: {
     backgroundColor: '#FFFFFF',
   },
-  warningText: {
-    marginTop: 64,
-    fontSize: 25,
-    textAlign: 'center',
-    color: '#4C2D73',
-//    height: 35,
-  },
   listView: {
     flex: 1
   }
